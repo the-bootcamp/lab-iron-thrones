6c9b73474da986160dfad25fcc52975975c5b74e
Nuno Pacheco <nunorpacheco@gmail.com>
2020-10-08 00:24:27 +0100

    Done

--- src/services/EpisodesService.js
+++ src/services/EpisodesService.js
@@ -4,14 +4,13 @@ const service = axios.create({
     baseURL: 'https://got-mock-api.herokuapp.com/episodes' 
 });
 
-export const getAllEpisodes = async () => {
-    try {
-      const response = await axios.get(
-        "https://got-mock-api.herokuapp.com/episodes"
-      );
-      const allEpisodesResponse = response.data;
-      return allEpisodesResponse;
-    } catch (error) {
-      return console.log(`Error while getting all episodes `, error);
-    }
-  };
+export const getAllEpisodes = () => {
+    return http.get('/episodes')
+        .then(response => response.data)
+}
+
+export const getSpecificEpisode = (id) => {
+    return http.get('/specific-episode')
+        .then(response => response.data)
+
+}
